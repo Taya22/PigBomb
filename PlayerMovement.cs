32c6563ccf8b1175a6dc69adf4954cb0a9c30fbc
Taya22 <64900670+Taya22@users.noreply.github.com>
2020-09-24 18:44:29 +0300

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] private float angleFactor = .3f; //смещение в сторону при движении вдоль оси Y
+
+    [Header("Input System")]
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    
+    
+    private Rigidbody2D rb;
+
+    private float inputHorizontal;
+    private float inputVertical;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+           MovePig(); 
+        }
+    }
+    
+    void MovePig()
+    {
+        inputHorizontal = SimpleInput.GetAxis( horizontalAxis ) * moveSpeed;
+        inputVertical = SimpleInput.GetAxis( verticalAxis ) * moveSpeed;
+    
+        if (inputVertical > 0)
+        {
+            rb.velocity = new Vector2(angleFactor, inputVertical);
+        }
+        else if (inputVertical < 0)
+        {
+            rb.velocity = new Vector2(-angleFactor, inputVertical);
+        }
+        else
+        {
+            rb.velocity = new Vector2(inputHorizontal, inputVertical);
+        }
+    }
+}
