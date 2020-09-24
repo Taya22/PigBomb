3c121a1aff4abcde0c028213645dafc8f127a0fd
Taya22 <64900670+Taya22@users.noreply.github.com>
2020-09-24 19:46:02 +0300

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -31,6 +31,10 @@ public class PlayerMovement : MonoBehaviour
         {
            MovePig(); 
         }
+        else
+        {
+           rb.velocity = new Vector2(0f, 0f);
+        }
     }
     
     void MovePig()
