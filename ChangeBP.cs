051a74e5089d8e6d58fd1b1b1c02b4c4aeb6d7fd
Taya22 <64900670+Taya22@users.noreply.github.com>
2020-09-28 13:15:39 +0300

    Script allows u to switch between two BombPlacers

---
+++ ChangeBP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeBP : MonoBehaviour
+{
+    [SerializeField] private Button waypointBP;
+    [SerializeField] private Button raycastBP;
+    [SerializeField] private Text currentBPText;
+    private bool raycastBPSetActive;
+
+    private void Start()
+    {
+       RaycastBPActive(false);
+    }
+
+    public void SwitchBP()
+    {
+        raycastBPSetActive = !raycastBPSetActive;
+        RaycastBPActive(raycastBPSetActive);
+        
+        var bombs = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach (var bomb in bombs)
+        {
+            Destroy(bomb);
+        }
+    }
+
+    private void RaycastBPActive(bool isActive)
+    {
+        raycastBP.gameObject.SetActive(isActive);
+        waypointBP.gameObject.SetActive(!isActive);
+        
+        if (isActive)
+        {
+            currentBPText.text = " Current BP = Raycast Bomb Placer";
+        }
+        else
+        {
+            currentBPText.text = " Current BP = Waypoint Bomb Placer";
+        }
+    }
+}
